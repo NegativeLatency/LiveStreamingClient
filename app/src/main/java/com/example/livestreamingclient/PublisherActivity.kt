f792fffcc63b4af685b5ec6a0a3bba211e64187a
WenheLI <wl1508@nyu.edu>
2020-03-19 23:10:43 +0800

    init

---
+++ app/src/main/java/com/example/livestreamingclient/PublisherActivity.kt
@@ -0,0 +1,10 @@
+package com.example.livestreamingclient
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PublisherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
