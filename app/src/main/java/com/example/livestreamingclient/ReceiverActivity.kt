42d76ef39f952688b0036239a6b7addf5f8f9e5f
ANPULI <al4902@nyu.edu>
2020-03-23 20:44:02 +0800

    add receiver demo

--- app/src/main/java/com/example/livestreamingclient/ReceiverActivity.kt
+++ app/src/main/java/com/example/livestreamingclient/ReceiverActivity.kt
@@ -1,10 +1,23 @@
 package com.example.livestreamingclient
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.shuyu.gsyvideoplayer.GSYBaseActivityDetail
+import com.shuyu.gsyvideoplayer.GSYVideoManager
+import com.shuyu.gsyvideoplayer.utils.OrientationUtils
+import com.shuyu.gsyvideoplayer.video.StandardGSYVideoPlayer
+import kotlinx.android.synthetic.main.activity_receiver.*
 
 class ReceiverActivity : AppCompatActivity() {
+    lateinit var videoPlayer: StandardGSYVideoPlayer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_receiver)
+
+        videoPlayer = rec_surface
+        var source1 = "http://9890.vod.myqcloud.com/9890_4e292f9a3dd011e6b4078980237cc3d3.f20.mp4"
+        videoPlayer.setUp(source1, true, "测试视频")
     }
 }
