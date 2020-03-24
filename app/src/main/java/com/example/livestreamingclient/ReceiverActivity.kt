53d15d29dce764690a6e6bf928b0b341469a7a95
ANPULI <al4902@nyu.edu>
2020-03-24 09:01:17 +0800

    add some interaction

--- app/src/main/java/com/example/livestreamingclient/ReceiverActivity.kt
+++ app/src/main/java/com/example/livestreamingclient/ReceiverActivity.kt
@@ -1,6 +1,8 @@
 package com.example.livestreamingclient
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.shuyu.gsyvideoplayer.GSYBaseActivityDetail
@@ -11,6 +13,7 @@ import kotlinx.android.synthetic.main.activity_receiver.*
 
 class ReceiverActivity : AppCompatActivity() {
     lateinit var videoPlayer: StandardGSYVideoPlayer
+    lateinit var orientationUtils: OrientationUtils
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +22,48 @@ class ReceiverActivity : AppCompatActivity() {
         videoPlayer = rec_surface
         var source1 = "http://9890.vod.myqcloud.com/9890_4e292f9a3dd011e6b4078980237cc3d3.f20.mp4"
         videoPlayer.setUp(source1, true, "测试视频")
+
+        videoPlayer.titleTextView.visibility = View.VISIBLE
+
+        videoPlayer.backButton.visibility = View.VISIBLE
+
+        orientationUtils = OrientationUtils(this, videoPlayer)
+
+        videoPlayer.fullscreenButton.setOnClickListener {
+            orientationUtils.resolveByClick()
+        }
+
+        videoPlayer.setIsTouchWiget(true)
+
+        videoPlayer.backButton.setOnClickListener {
+            onBackPressed()
+        }
+
+        videoPlayer.startPlayLogic()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        videoPlayer.onVideoPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        videoPlayer.onVideoResume()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        GSYVideoManager.releaseAllVideos()
+        orientationUtils.releaseListener()
+    }
+
+    override fun onBackPressed() {
+        if (orientationUtils.screenType == ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE) {
+            videoPlayer.fullscreenButton.performClick()
+            return
+        }
+        videoPlayer.setVideoAllCallBack(null)
+        super.onBackPressed()
     }
 }
