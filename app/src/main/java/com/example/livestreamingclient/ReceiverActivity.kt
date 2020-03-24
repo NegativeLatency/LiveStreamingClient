f9c377d246047352d0c4bee9e8523041f762097e
ANPULI <al4902@nyu.edu>
2020-03-24 11:46:04 +0800

    use button to activate

--- app/src/main/java/com/example/livestreamingclient/ReceiverActivity.kt
+++ app/src/main/java/com/example/livestreamingclient/ReceiverActivity.kt
@@ -20,8 +20,10 @@ class ReceiverActivity : AppCompatActivity() {
         setContentView(R.layout.activity_receiver)
 
         videoPlayer = rec_surface
-        var source1 = "http://9890.vod.myqcloud.com/9890_4e292f9a3dd011e6b4078980237cc3d3.f20.mp4"
-        videoPlayer.setUp(source1, true, "测试视频")
+        rec_stream_btn.setOnClickListener {
+            videoPlayer.setUp(rec_addr_input.text.toString(), true, "测试视频")
+            videoPlayer.startPlayLogic()
+        }
 
         videoPlayer.titleTextView.visibility = View.VISIBLE
 
@@ -39,7 +41,6 @@ class ReceiverActivity : AppCompatActivity() {
             onBackPressed()
         }
 
-        videoPlayer.startPlayLogic()
     }
 
     override fun onPause() {
