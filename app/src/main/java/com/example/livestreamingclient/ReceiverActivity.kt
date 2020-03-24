14613d8ca6b8762abfa05886b1f695b1351fc3f0
Anpu Li <al4902@nyu.edu>
2020-03-24 11:53:09 +0800

    Add receiver demo (#3)
    * add receiver demo
    
    * add some interaction
    
    * use button to activate
    
    * downgrade gradle

--- app/src/main/java/com/example/livestreamingclient/ReceiverActivity.kt
+++ app/src/main/java/com/example/livestreamingclient/ReceiverActivity.kt
@@ -1,10 +1,70 @@
 package com.example.livestreamingclient
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.shuyu.gsyvideoplayer.GSYBaseActivityDetail
+import com.shuyu.gsyvideoplayer.GSYVideoManager
+import com.shuyu.gsyvideoplayer.utils.OrientationUtils
+import com.shuyu.gsyvideoplayer.video.StandardGSYVideoPlayer
+import kotlinx.android.synthetic.main.activity_receiver.*
 
 class ReceiverActivity : AppCompatActivity() {
+    lateinit var videoPlayer: StandardGSYVideoPlayer
+    lateinit var orientationUtils: OrientationUtils
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_receiver)
+
+        videoPlayer = rec_surface
+        rec_stream_btn.setOnClickListener {
+            videoPlayer.setUp(rec_addr_input.text.toString(), true, "测试视频")
+            videoPlayer.startPlayLogic()
+        }
+
+        videoPlayer.titleTextView.visibility = View.VISIBLE
+
+        videoPlayer.backButton.visibility = View.VISIBLE
+
+        orientationUtils = OrientationUtils(this, videoPlayer)
+
+        videoPlayer.fullscreenButton.setOnClickListener {
+            orientationUtils.resolveByClick()
+        }
+
+        videoPlayer.setIsTouchWiget(true)
+
+        videoPlayer.backButton.setOnClickListener {
+            onBackPressed()
+        }
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        videoPlayer.onVideoPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        videoPlayer.onVideoResume()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        GSYVideoManager.releaseAllVideos()
+        orientationUtils.releaseListener()
+    }
+
+    override fun onBackPressed() {
+        if (orientationUtils.screenType == ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE) {
+            videoPlayer.fullscreenButton.performClick()
+            return
+        }
+        videoPlayer.setVideoAllCallBack(null)
+        super.onBackPressed()
     }
 }
